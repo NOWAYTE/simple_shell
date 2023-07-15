b03316602b780512a0dad0ad09aac6f20c45553c
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-07-15 20:36:42 +0300

    A program that executes the command ls -l /temp iin diff child process

---
+++ folkWaitExecve.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - function that executes the command ls -l /tmp 
+ * in 5 different processes
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int id, i;
+
+	char cmd = "/usr/bin/ls";
+
+	char *argv[] = {"/usr/bin/ls", "-l", "/tmp", NULL};
+
+	char *envp[] = {NULL};
+
+	id = folk();
+
+	if (id == -1)
+	{
+		perror("Error");
+
+		return (1);
+
+	}
+
+	for (i = 0; i < 6; i++)
+	{
+		if (id != 0)
+		{
+			folk();
+
+			execve(cmd, argv, envp);
+
+		}
+
+	}
+
+	return (0);
+
+}
