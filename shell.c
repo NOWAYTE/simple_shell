992d6e20b9deb154ce17393a63c9169fce9bed55
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-07-17 10:18:18 +0300

    Fix code errors

--- shell.c
+++ shell.c
@@ -8,50 +8,51 @@ int main(void)
 {
 
 	char *cmd = NULL;
-	int argc = 0;
-	int n = 0;
-	int x = 0;
-	char **argv = NULL;
-
 	size_t i = 0;
-
-	char *token = NULL;
-
 	ssize_t read_n;
+	int argc = 0;
+	char **argv = NULL;
+	int n = 0;
+	char *token = NULL;
+	char *delim = NULL;
 
-	char *delim = " ";
 
 	printf("#cisfun$ ");
 
-	while ((read_n = getline(&cmd, &i, stdin)) == -1)
+	while ((read_n = getline(&cmd, &i, stdin)) != -1)
 	{
-		return (-1);
+		token = strtok(cmd, delim);
+
+		while (token)
+		{
+			argc++;
+
+			token = strtok(NULL, delim);
+
+		}
+
+		argv = malloc(sizeof(char *) * argc);
+
+		token = strtok(cmd, delim);
+
+		while (token)
+		{
+			argv[n] = token;
+			n++;
+
+			token = strtok(NULL, delim);
+
+		}
+
+		free(argv);
+
+		printf("#cisfun$ ");
+
 	}
 
-	token = strtok(cmd, delim);
-
-	while (token)
-	{
-		token = strtok(NULL, delim);
-		argc++;
-	}
-
-	argv = malloc(sizeof(char *) *argc);
-
-	while (token)
-	{
-		argv[n] = token;
-
-		token = strtok(NULL, delim);
-		n++;
-	}
-
-	while (argv)
-	{
-		printf("%s", argv[x]);
-		x++;
-	}
+	free(cmd);
 
 	return (0);
 
 }
+
