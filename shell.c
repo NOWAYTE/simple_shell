36d2ca4efc1ffe7e5d85df9fe66c049d434c846c
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-07-25 19:35:32 +0300

    Code update

--- shell.c
+++ shell.c
@@ -54,6 +54,53 @@ int main(__attribute__((unused))int argc, char **argv, char **envp)
 		}
 
 		argV[x] = NULL;
+
+		path = _getenv("PATH");
+
+		dir = strtok(path, ":");
+
+		while (dir != NULL)
+		{
+			dp = opendir(dir);
+
+			if (dp != NULL)
+			{
+				while ((entry = readdir(dp)) != NULL)
+				{
+					if ((access(entry->d_name, X_OK)) == 0)
+					{
+						if ((_strcmp(entry->d_name, argV[0])) == 0)
+						{
+							id = fork();
+
+							if (id != 0)
+							{
+								wait(0);
+							}
+							else
+							{
+								if (execve(entry->d_name, argV, envp) == -1)
+								{
+									_print(argv[0], STDOUT_FILENO);
+									_print(": ", STDOUT_FILENO);
+									_print("No such file  or directory \n", STDOUT_FILENO);
+
+								}
+							}
+						}
+					}
+				}
+				closedir(dp);
+
+			}
+
+			dir = strtok(NULL, ":");
+
+		}
+
+		_print("($)", STDOUT_FILENO);
+
+
 		
 		argC = 0;
 		x = 0;
