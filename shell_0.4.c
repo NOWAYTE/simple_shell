5fbbf1334c4a846759894a91a2df20358ccab0ee
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-07-25 15:06:52 +0300

    Fix code errors

--- shell_0.4.c
+++ shell_0.4.c
@@ -54,6 +54,52 @@ int main(__attribute__((unused))int argc, char **argv, char **envp)
 		}
 
 		argV[x] = NULL;
+
+		path = _getenv("PATH");
+
+		dir = strtok(path, " : ");
+
+		while (dir != NULL)
+		{
+			dp = opendir(dir);
+
+			if (dp != NULL)
+			{
+				while ((entry = readdir(dp)) != NULL)
+				{
+					if((_strcmp(entry->d_name, argV[0])) == 0 && (access(entry->d_name, X_OK)) == 0)
+					{
+						id = fork();
+
+						if (id != 0)
+						{
+							wait(0);
+						}
+
+						else
+						{
+							if (execve(entry->d_name, argV, envp) == -1)
+							{
+								_print(argv[0], STDOUT_FILENO);
+								_print(": ", STDOUT_FILENO);
+								_print("No such file or directory \n", STDOUT_FILENO);
+
+							}
+							else
+							{
+								_print("No such file or directory.. something went wrong ", STDOUT_FILENO);
+							}
+						}
+					}
+				}
+
+				closedir(dp);
+			}
+
+			dir = strtok(NULL, ":");
+		}
+
+		_print("($)", STDOUT_FILENO);
 		
 		argC = 0;
 		x = 0;
