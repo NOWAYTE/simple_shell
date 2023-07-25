04289e2fa8bddd92687baf51ebfd838f43761481
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-07-25 12:22:44 +0300

    Code update

--- path_0.1.c
+++ path_0.1.c
@@ -1,48 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include<string.h>
 #include<dirent.h>
-#include<unistd.h>
 
-void print_path()
+void path()
 {
 	char *path;
 	char *token;
 	DIR *dp;
+
 	struct dirent *entry;
 
 	path = getenv("PATH");
 
-	token = strtok(path, " : ");
+	token = strtok(path, ":");
 
 	while (token)
 	{
 		dp = opendir(token);
 
-		if(dp != NULL)
+		if (dp != NULL)
 		{
 			while ((entry = readdir(dp)) != NULL)
 			{
-
-				if (access(entry->d_name, X_OK) == 0)
+				if (strcmp(entry->d_name, "ls") == 0 && access(entry->d_name, X_OK) == 0)
 				{
-					if (strcmp(entry->d_name, "ls") == 0)
-					{
-						printf("%s", entry->d_name);
+					printf("%s/%s \n", token, entry->d_name);
 
-					}
 				}
-			}
-		}
-	}
 
-	token = strtok(NULL, " : ");
+			}
+
+			closedir(dp);
+		}
+
+		token = strtok(NULL, ":");
+
+	}
 
 }
 
 int main()
 {
-	print_path();
+	path();
+
 
 	return (0);
 
