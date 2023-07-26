90c1895241c06294cc10ddf40e9086c715acf215
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-07-27 02:20:41 +0300

    Code update

---
+++ built1.c
@@ -0,0 +1,11 @@
+#include "header.h"
+/**
+ * sh_alias - checks for an alias
+ * @argv: arguments
+ * Return: 0
+**/
+int sh_alias(char **argv)
+{
+	argv = argv;
+	return (0);
+}
--- shell.c
+++ shell.c
@@ -1,65 +1,173 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "header.h"
 
-#define MAX_COMMAND_LENGTH 100
-#define MAX_ARGUMENTS_COUNT 10
-
-void prompt() {
-    printf("#cisfun$ ");
+/**
+ * main - main function for our simple shell
+ * description: main
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	shell_loop(argc, argv);
+	return (0);
 }
 
-char* read_command() {
-    char* command = malloc(MAX_COMMAND_LENGTH * sizeof(char));
-    fgets(command, MAX_COMMAND_LENGTH, stdin);
-    return command;
+/**
+ * shell_loop - shell loop to constantly run the shell
+ * description: until the exit command is entered
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+int shell_loop(int argc, char **argv)
+{
+	int userinput;
+	char *buffer = NULL;
+	size_t bufsize;
+	char *prompt = "$ ";
+	char **path_tokens = NULL;
+	char *executable = NULL;
+
+	(void) argc;
+
+	buffer = NULL;
+	while (1)
+	{
+		/* buffer = malloc(sizeof(char) * bufsize);*/
+		   /* if (buffer == NULL) */
+		   /* perror("Malloc failure\n"); */
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, prompt, stringlength(prompt));
+		}
+		userinput = getline(&buffer, &bufsize, stdin);
+		if (userinput == -1)
+			break;
+		argv = tokenize(buffer);
+		if (argv[0] == NULL)
+			continue;
+		if (function_finder(argv, buffer) == 1)
+		{
+			free(argv);
+			continue;
+		}
+		path_tokens = _get_env("PATH");
+		executable = dir_search(argv, path_tokens);
+		executor(executable, argv);
+		dubbie_free(path_tokens);
+		if (argv[0][0] != '/')
+			free(executable);
+		free(argv);
+	}
+	free(buffer);
+return (0);
 }
 
-void execute_command(char* command) {
-    char* arguments[MAX_ARGUMENTS_COUNT];
-    int argument_count = 0;
+/**
+ * tokenize - tokenize user input
+ * description: to breakdown command
+ * @userinput: userinput
+ * Return: 0
+ */
+char **tokenize(char *userinput)
+{
+	int token_inc = 0;
+	char *tokenize = NULL;
+	char **argv = NULL;
+	int tokencount = 0;
+	int i;
 
-    // split the command into arguments
-    char* argument = strtok(command, " \t\n");
-    while (argument != NULL) {
-        arguments[argument_count++] = argument;
-        argument = strtok(NULL, " \t\n");
-    }
-    arguments[argument_count] = NULL;
+	strtok(userinput, "\n");
+	for (i = 0; userinput[i] != '\0'; i++)
+	{
+		if (userinput[i] == ' ')
+		{
+			tokencount++;
+		}
+	}
+	argv = malloc(8 * (tokencount + 2));
+	if (argv != NULL)
+	{
+		token_inc = 0;
+		tokenize = strtok(userinput, " ");
+		while (token_inc < (tokencount + 1))
+		{
+			argv[token_inc] = tokenize;
+			tokenize = strtok(NULL, " ");
+			token_inc++;
+		}
+		argv[token_inc] = NULL;
+	}
 
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        printf("Error: fork failed\n");
-        exit(1);
-    } else if (pid == 0) {
-        // child process
-        if (execvp(arguments[0], arguments) < 0) {
-            printf("Error: command not found\n");
-            exit(1);
-        }
-    } else {
-        // parent process
-        wait(NULL);
-    }
+	/*free(userinput);*/
+	return (argv);
 }
 
-int main() {
-    while (1) {
-        prompt();
+/**
+ * executor - executes the command given
+ * description: by forking and execing
+ * @asdf: asdf
+ * @argv: argv
+ * Return: 0
+ */
 
-        char* command = read_command();
-        if (strlen(command) == 0) {
-            // empty input
-            continue;
-        }
+int executor(char *asdf, char **argv)
+{
+	pid_t child_pid;
 
-        execute_command(command);
-
-        free(command);
-    }
-
-    return 0;
+	child_pid = fork();
+	if (child_pid == -1)
+		perror("Fork failure\n");
+	if (child_pid == 0)
+	{
+		execve(asdf, argv, environ);
+	}
+	else
+	{
+		wait(NULL);
+	}
+	/*free(argv);*/
+	return (0);
 }
+
+
+/**
+ * function_finder - for builtins
+ * description: searches and compares to see if the command is a builtin
+ * @argv: argv
+ * @buffer: buffer
+ * Return: 0
+ */
+int function_finder(char **argv, char *buffer)
+{
+	int i;
+
+	builtins arr[] = {
+		{"cd", sh_cd},
+		{"env", sh_env},
+		{"exit", sh_exit},
+		{"setenv", sh_setenv},
+		{"unsetenv", sh_unsetenv},
+		{"alias", sh_alias},
+		{'\0', NULL}
+	};
+
+	if (argv != NULL)
+	{
+		if (_strcmp(argv[0], "exit") == 0)
+		{
+			sh_exit(argv, buffer);
+			return (1);
+		}
+		for (i = 0; arr[i].func; i++)
+		{
+			if (_strcmp(argv[0], arr[i].argv) == 0)
+			{
+				arr[i].func();
+				return (1);
+			}
+		}
+	}
+	return (0);
+}oi
